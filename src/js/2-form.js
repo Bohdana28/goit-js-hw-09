14ee9b568089aea84453588e1d95e0cb0e91f2e0
Bohdana28 <bog.olhowetzcka@gmail.com>
2024-05-20 21:52:33 +0100

    form js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,46 +1,35 @@
-let formData = {
-  email: '',
-  message: '',
+const formRefs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[name="email"]'),
+  message: document.querySelector('textarea[name="message"]'),
 };
 
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
+
 function saveFormData() {
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  const formData = {
+    email: formRefs.email.value,
+    message: formRefs.message.value,
+  };
+  localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(formData));
 }
 
-function loadFormData() {
-  const savedFormData = localStorage.getItem('feedback-form-state');
-  if (savedFormData) {
-    formData = JSON.parse(savedFormData);
-    document.querySelector('input[name="email"]').value = formData.email;
-    document.querySelector('textarea[name="message"]').value = formData.message;
+formRefs.form.addEventListener('input', saveFormData);
+
+if (localStorage.getItem(FEEDBACK_FORM_KEY)) {
+  const data = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+  formRefs.email.value = data.email || '';
+  formRefs.message.value = data.message || '';
+}
+
+formRefs.form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formRefs.email.value.trim() === '' || formRefs.message.value.trim() === '') {
+    alert('Please, fill in all the fields');
+  } else {
+    const data = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+    localStorage.removeItem(FEEDBACK_FORM_KEY);
+    formRefs.form.reset();
+    console.log(data); 
   }
-}
-
-function clearFormData() {
-  localStorage.removeItem('feedback-form-state');
-  formData = { email: '', message: '' };
-  document.querySelector('input[name="email"]').value = '';
-  document.querySelector('textarea[name="message"]').value = '';
-}
-
-window.addEventListener('load', loadFormData);
-
-document
-  .querySelector('.feedback-form')
-  .addEventListener('input', function (event) {
-    formData[event.target.name] = event.target.value;
-    saveFormData();
-  });
-
-document
-  .querySelector('.feedback-form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    if (formData.email === '' || formData.message === '') {
-      alert('Fill please all fields');
-    } else {
-      console.log(formData);
-      clearFormData();
-    }
-  });
+});
